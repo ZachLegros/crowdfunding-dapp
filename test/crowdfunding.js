c19a8f89a8b91a8bfa79a03b0da91f88e29e0b8d
zachlegros <legroszach3921@gmail.com>
2021-04-04 21:11:36 -0400

    Added minimum donation and initial contributions test and removed chai

--- test/crowdfunding.js
+++ test/crowdfunding.js
@@ -1,29 +1,43 @@
 const CrowdfundingFactory = artifacts.require("CrowdfundingFactory");
 const Crowdfunding = artifacts.require("Crowdfunding");
 
-contract("Crowdfunding", (accounts) => {
+contract("CrowdfundingFactory", async (accounts) => {
   const defaultMin = 100;
+  let factory;
 
-  it("should have the corresponding campaign owner address", async () => {
-    const crowdfundingFactoryInstance = await CrowdfundingFactory.deployed();
-    await crowdfundingFactoryInstance.createCampaign(defaultMin);
-    const deployedCampaigns = await crowdfundingFactoryInstance.getDeployedCampaigns();
+  before(async () => {
+    factory = await CrowdfundingFactory.deployed();
+  });
+
+  it("has correct owner address", async () => {
+    await factory.createCampaign(defaultMin);
+    const deployedCampaigns = await factory.getDeployedCampaigns();
     const campaign = await Crowdfunding.at(deployedCampaigns[0]);
-
     const owner = await campaign.owner();
-    const minimum = await campaign.minimumDonation();
-    const donatorsCount = await campaign.donatorsCount();
-
     assert.equal(
       owner,
       accounts[0],
       "The first account was not the owner of the campaign"
     );
+  });
+
+  it("has default minimum donation value", async () => {
+    await factory.createCampaign(defaultMin);
+    const deployedCampaigns = await factory.getDeployedCampaigns();
+    const campaign = await Crowdfunding.at(deployedCampaigns[0]);
+    const minimum = await campaign.minimumDonation();
     assert.equal(
       defaultMin,
       minimum,
       "The minimum donation is not the default value given"
     );
+  });
+
+  it("has 0 initial donations", async () => {
+    await factory.createCampaign(defaultMin);
+    const deployedCampaigns = await factory.getDeployedCampaigns();
+    const campaign = await Crowdfunding.at(deployedCampaigns[0]);
+    const donatorsCount = await campaign.donatorsCount();
     assert.equal(donatorsCount, 0, "The initial donators count is not 0");
   });
 });
--- truffle-config.js
+++ truffle-config.js
@@ -14,6 +14,10 @@ module.exports = {
   compilers: {
     solc: {
       version: "^0.8.0", // A version or constraint - Ex. "^0.5.0"
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
     },
   },
 };
