0e336afbb9eec8dc1e971af64b4a6b1bffeff1aa
zachlegros <legroszach3921@gmail.com>
2021-04-04 20:56:57 -0400

    Added chai as npm dependency

--- test/crowdfunding.js
+++ test/crowdfunding.js
@@ -2,18 +2,28 @@ const CrowdfundingFactory = artifacts.require("CrowdfundingFactory");
 const Crowdfunding = artifacts.require("Crowdfunding");
 
 contract("Crowdfunding", (accounts) => {
+  const defaultMin = 100;
+
   it("should have the corresponding campaign owner address", async () => {
     const crowdfundingFactoryInstance = await CrowdfundingFactory.deployed();
-    await crowdfundingFactoryInstance.createCampaign(100);
+    await crowdfundingFactoryInstance.createCampaign(defaultMin);
     const deployedCampaigns = await crowdfundingFactoryInstance.getDeployedCampaigns();
     const campaign = await Crowdfunding.at(deployedCampaigns[0]);
 
     const owner = await campaign.owner();
+    const minimum = await campaign.minimumDonation();
+    const donatorsCount = await campaign.donatorsCount();
 
     assert.equal(
       owner,
       accounts[0],
       "The first account was not the owner of the campaign"
     );
+    assert.equal(
+      defaultMin,
+      minimum,
+      "The minimum donation is not the default value given"
+    );
+    assert.equal(donatorsCount, 0, "The initial donators count is not 0");
   });
 });
