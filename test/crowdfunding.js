b94617f9109a4746fb203f99341b0b7d5d278df7
zachlegros <legroszach3921@gmail.com>
2021-04-05 20:05:33 -0400

    Added 'cannot withdraw more than what the contract holds' test

--- test/crowdfunding.js
+++ test/crowdfunding.js
@@ -5,6 +5,7 @@ const Crowdfunding = artifacts.require("Crowdfunding");
 
 contract("CrowdfundingFactory", async (accounts) => {
   const defaultMin = 5000000000000000; // in wei
+  let lastCampaignIndex = -1;
   let factory;
 
   before(async () => {
@@ -13,8 +14,11 @@ contract("CrowdfundingFactory", async (accounts) => {
 
   it("has correct owner address", async () => {
     await factory.createCampaign(defaultMin);
+    lastCampaignIndex++;
     const deployedCampaigns = await factory.getDeployedCampaigns();
-    const campaign = await Crowdfunding.at(deployedCampaigns[0]);
+    const campaign = await Crowdfunding.at(
+      deployedCampaigns[lastCampaignIndex]
+    );
     const owner = await campaign.owner();
     assert.equal(
       owner,
@@ -25,8 +29,11 @@ contract("CrowdfundingFactory", async (accounts) => {
 
   it("has default minimum donation value", async () => {
     await factory.createCampaign(defaultMin);
+    lastCampaignIndex++;
     const deployedCampaigns = await factory.getDeployedCampaigns();
-    const campaign = await Crowdfunding.at(deployedCampaigns[0]);
+    const campaign = await Crowdfunding.at(
+      deployedCampaigns[lastCampaignIndex]
+    );
 
     const minimum = await campaign.minimumDonation();
     assert.equal(
@@ -38,8 +45,11 @@ contract("CrowdfundingFactory", async (accounts) => {
 
   it("has 0 initial donations", async () => {
     await factory.createCampaign(defaultMin);
+    lastCampaignIndex++;
     const deployedCampaigns = await factory.getDeployedCampaigns();
-    const campaign = await Crowdfunding.at(deployedCampaigns[0]);
+    const campaign = await Crowdfunding.at(
+      deployedCampaigns[lastCampaignIndex]
+    );
 
     const donatorsCount = await campaign.donatorsCount();
     assert.equal(donatorsCount, 0, "The initial donators count is not 0");
@@ -47,8 +57,11 @@ contract("CrowdfundingFactory", async (accounts) => {
 
   it("cannot donate to own campaign", async () => {
     await factory.createCampaign(defaultMin);
+    lastCampaignIndex++;
     const deployedCampaigns = await factory.getDeployedCampaigns();
-    const campaign = await Crowdfunding.at(deployedCampaigns[0]);
+    const campaign = await Crowdfunding.at(
+      deployedCampaigns[lastCampaignIndex]
+    );
 
     await tryCatch(
       campaign.send(defaultMin, { from: accounts[0] }),
@@ -58,8 +71,11 @@ contract("CrowdfundingFactory", async (accounts) => {
 
   it("donate as non owner", async () => {
     await factory.createCampaign(defaultMin);
+    lastCampaignIndex++;
     const deployedCampaigns = await factory.getDeployedCampaigns();
-    const campaign = await Crowdfunding.at(deployedCampaigns[0]);
+    const campaign = await Crowdfunding.at(
+      deployedCampaigns[lastCampaignIndex]
+    );
 
     await campaign.send(defaultMin, { from: accounts[1] });
     const balance = await web3.eth.getBalance(campaign.address);
@@ -71,5 +87,30 @@ contract("CrowdfundingFactory", async (accounts) => {
       "The balance of the campaign did not update properly"
     );
     assert.equal(donatorsCount, 1, "The donators count was not incremented");
+  });
+
+  it("has minimum donation", async () => {
+    await factory.createCampaign(defaultMin);
+    lastCampaignIndex++;
+    const deployedCampaigns = await factory.getDeployedCampaigns();
+    const campaign = await Crowdfunding.at(
+      deployedCampaigns[lastCampaignIndex]
+    );
+
+    await tryCatch(
+      campaign.send(defaultMin / 5, { from: accounts[1] }),
+      errTypes.revert
+    );
+  });
+
+  it("cannot withdraw more than what the contract holds", async () => {
+    await factory.createCampaign(defaultMin);
+    lastCampaignIndex++;
+    const deployedCampaigns = await factory.getDeployedCampaigns();
+    const campaign = await Crowdfunding.at(
+      deployedCampaigns[lastCampaignIndex]
+    );
+
+    await tryCatch(campaign.withdraw(defaultMin, accounts[2]), errTypes.revert);
   });
 });
